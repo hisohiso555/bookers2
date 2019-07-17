4dcc3d84ff5149a77a2d5a5698e2e0f333f7c910
unknown <hisohiso555>
2019-07-17 21:45:39 +0900

    fix

--- db/migrate/20190524200619_create_books.rb
+++ db/migrate/20190524200619_create_books.rb
@@ -1,6 +1,6 @@
 class CreateBooks < ActiveRecord::Migration[5.2]
   def change
-    create_table :books :options => 'ENGINE=InnoDB ROW_FORMAT=DYNAMIC' do |t|
+    create_table :books, :options => 'ENGINE=InnoDB ROW_FORMAT=DYNAMIC' do |t|
       t.text :title
       t.text :body
       t.integer :user_id
--- db/migrate/20190602022654_create_book_comments.rb
+++ db/migrate/20190602022654_create_book_comments.rb
@@ -1,6 +1,6 @@
 class CreateBookComments < ActiveRecord::Migration[5.2]
   def change
-    create_table :book_comments :options => 'ENGINE=InnoDB ROW_FORMAT=DYNAMIC' do |t|
+    create_table :book_comments, :options => 'ENGINE=InnoDB ROW_FORMAT=DYNAMIC' do |t|
       t.text :comment
       t.integer :use_id
       t.integer :book_id
